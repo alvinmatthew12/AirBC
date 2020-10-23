3fe6bb225417ad8bb066d031d944e76363e40a65
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-10-23 22:45:24 +0700

    Initial Commit

---
+++ AirBC WatchKit App WatchKit Extension/FlightInterfaceController.swift
@@ -0,0 +1,46 @@
+//
+//  FlightInterfaceController.swift
+//  AirBC WatchKit App WatchKit Extension
+//
+//  Created by Alvin Matthew Pratama on 23/10/20.
+//
+
+import WatchKit
+
+class FlightInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var flightLabel: WKInterfaceLabel!
+    @IBOutlet var routeLabel: WKInterfaceLabel!
+    @IBOutlet var boardingLabel: WKInterfaceLabel!
+    @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var gateLabel: WKInterfaceLabel!
+    @IBOutlet var seatLabel: WKInterfaceLabel!
+    
+    var flight: Flight? {
+        didSet {
+            guard let flight = flight else { return }
+            
+            flightLabel.setText("Flight \(flight.shortNumber)")
+            routeLabel.setText(flight.route)
+            boardingLabel.setText("\(flight.number) Boards")
+            boardTimeLabel.setText(flight.boardsAt)
+            
+            if flight.onSchedule {
+                statusLabel.setText("On Time")
+            } else {
+                statusLabel.setText("Delayed")
+                statusLabel.setTextColor(.red)
+            }
+            gateLabel.setText("Gate \(flight.gate)")
+            seatLabel.setText("Seat \(flight.seat)")
+        }
+    }
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        flight = Flight.allFlights().first
+    }
+    
+}
---
+++ AirBC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AirBC
+//
+//  Created by Alvin Matthew Pratama on 23/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
