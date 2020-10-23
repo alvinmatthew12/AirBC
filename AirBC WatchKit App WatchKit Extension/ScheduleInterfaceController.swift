3ebbb0f73cc902a8460fbe3c21b1ecfa90949e74
Alvin Matthew Pratama <alvinmatthew12@gmail.com>
2020-10-24 00:54:04 +0700

    Create image-based animation, use watchOS animation API, update UI and API

--- AirBC WatchKit App WatchKit Extension/ScheduleInterfaceController.swift
+++ AirBC WatchKit App WatchKit Extension/ScheduleInterfaceController.swift
@@ -13,6 +13,7 @@ class ScheduleInterfaceController: WKInterfaceController {
     @IBOutlet weak var flightsTable: WKInterfaceTable!
     
     var flights = Flight.allFlights()
+    var selectedIndex = 0
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
@@ -25,9 +26,21 @@ class ScheduleInterfaceController: WKInterfaceController {
         }
     }
     
+    override func didAppear() {
+        super.didAppear()
+        
+        guard flights[selectedIndex].checkedIn, let controller = flightsTable.rowController(at: selectedIndex) as? FlightRowController else { return }
+        
+        animate(withDuration: 0.35) {
+            controller.updateForCheckIn()
+        }
+    }
+    
     override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
         let flight = flights[rowIndex]
-        presentController(withName: "Flight", context: flight)
+        let controllers = ["Flight", "CheckIn"]
+        selectedIndex = rowIndex
+        presentController(withNames: controllers, contexts: [flight, flight])
     }
 
 }
